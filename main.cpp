2ad4393148465802f5c6bdaf93a6552d31d1d051
Sarthak <ss22072000@gmail.com>
2024-04-22 09:43:36 +0530

    feat: Adding the ball movement and collision logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include "raylib.h"
 
+struct Ball
+{
+    float x{}, y{};
+    float speedX{}, speedY{};
+    float radius{};
+
+    void draw()
+    {
+        DrawCircle((int)x, (int)y, radius, WHITE);
+    }
+};
+
 int main()
 {
     std::cout << "Welcome to Pong! \n";
@@ -8,34 +20,43 @@ int main()
 
     SetWindowState(FLAG_VSYNC_HINT);
 
-    float ballX = GetScreenWidth() / 2.0f, ballY = GetScreenHeight() / 2.0f;
-    float ballRadius = 5;
-    float paddle1X = 0, paddle2X = 0, paddle1Y = 0, paddle2Y = 0;
-    float ballSpeedX = 50.0f;
-    float ballSpeedY = 50.0f;
-
-    std::cout << ballX << ballY;
-    std::cout << paddle1X << paddle2X << paddle2Y << paddle1Y;
+    // Initializing the ball state
+    Ball ball{};
+    ball.x = GetScreenWidth() / 2.0f;
+    ball.y = GetScreenHeight() / 2.0f;
+    ball.radius = 5.0f;
+    ball.speedX = 200.0f;
+    ball.speedY = 200.0f;
 
     // Game Loop
     while (!WindowShouldClose())
     {
-        
-        // Game Update Logic
-        ballX += ballSpeedX * GetFrameTime();
-        ballY += ballSpeedY * GetFrameTime();
 
+        // Ball Movement and Boundary Collision Logic
+        ball.x += ball.speedX * GetFrameTime();
+        ball.y += ball.speedY * GetFrameTime();
 
-        // Rendering Logic
+        if (ball.y < 0 || ball.y >= GetScreenHeight())
+        {
+            ball.speedY *= -1;
+        }
+
+        if (ball.x < 0 || ball.x > GetScreenWidth())
+        {
+
+            ball.speedX *= -1;
+        }
+
+        // Rendering
+        // Game updates should be made before the BeginDrawing() call
         BeginDrawing();
 
         ClearBackground(BLACK);
         DrawFPS(10, 10);
 
-        DrawCircle((int)ballX, (int)ballY, ballRadius, WHITE);
         DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, RED);
         DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, RED);
-
+        ball.draw();
         EndDrawing();
     }
 
