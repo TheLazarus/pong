704ec7fed5015c266ba836cab827e9979150bc8c
Sarthak <ss22072000@gmail.com>
2024-04-22 10:16:40 +0530

    feat: Refactoring paddle code

--- main.cpp
+++ main.cpp
@@ -3,15 +3,37 @@
 
 struct Ball
 {
-    float x, y;
-    float speedX, speedY;
-    float radius;
+    float x, y, speedX, speedY;
+    int radius;
 
     void draw()
     {
         DrawCircle((int)x, (int)y, radius, WHITE);
     }
 };
+
+struct Paddle
+{
+    float x, y, speed;
+    int width, height;
+
+    void draw()
+    {
+        DrawRectangle(x, y, width, height, RED);
+    }
+};
+
+struct Player
+{
+    int points;
+};
+
+void initializeWindow()
+{
+
+    ClearBackground(BLACK);
+    DrawFPS(10, 10);
+}
 
 int main()
 {
@@ -24,9 +46,17 @@ int main()
     Ball ball{};
     ball.x = GetScreenWidth() / 2.0f;
     ball.y = GetScreenHeight() / 2.0f;
-    ball.radius = 5.0f;
+    ball.radius = 5;
     ball.speedX = 200.0f;
     ball.speedY = 200.0f;
+
+    // Initializing the paddles
+    Paddle paddle1{.x{50.0f}, .y{GetScreenHeight() / 2.0f - 50.0f}, .speed{400}, .width{10}, .height{100}};
+    Paddle paddle2{.x{GetScreenWidth() - 50 - 10}, .y{GetScreenHeight() / 2 - 50}, .speed{400}, .width{10}, .height{100}};
+
+    // Initializing the players
+    Player player1{};
+    Player player2{};
 
     // Game Loop
     while (!WindowShouldClose())
@@ -47,15 +77,31 @@ int main()
             ball.speedX *= -1;
         }
 
+        // Paddle Movement Logic
+        if (IsKeyDown(KEY_D))
+        {
+            paddle1.y -= paddle1.speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_F))
+        {
+            paddle1.y += paddle1.speed * GetFrameTime();
+        }
+
+        if (IsKeyDown(KEY_J))
+        {
+            paddle2.y -= paddle2.speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_K))
+        {
+            paddle2.y += paddle2.speed * GetFrameTime();
+        }
+
         // Rendering
         // Game updates should be made before the BeginDrawing() call
         BeginDrawing();
-
-        ClearBackground(BLACK);
-        DrawFPS(10, 10);
-
-        DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, RED);
-        DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10, 100, RED);
+        initializeWindow();
+        paddle1.draw();
+        paddle2.draw();
         ball.draw();
         EndDrawing();
     }
