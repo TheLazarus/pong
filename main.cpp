0616bd6fd90ca00ffc338c62edc404a24672d144
Sarthak <ss22072000@gmail.com>
2024-04-23 09:00:53 +0530

    feat: Adding collision logic

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 struct Ball
 {
-    float x, y, speedX, speedY;
+    float x, y;
+    float speedX, speedY;
     int radius;
 
     void draw()
@@ -15,11 +16,16 @@ struct Ball
 struct Paddle
 {
     float x, y, speed;
-    int width, height;
+    float width, height;
+
+    Rectangle GetRect()
+    {
+        return Rectangle{x - width / 2, y - height / 2, width, height};
+    }
 
     void draw()
     {
-        DrawRectangle(x - width / 2, y - height / 2, width, height, RED);
+        DrawRectangleRec(GetRect(), RED);
     }
 };
 
@@ -36,7 +42,7 @@ int main()
     SetWindowState(FLAG_VSYNC_HINT);
 
     // Initializing the ball state
- 
+
     Ball ball{
         .x{GetScreenWidth() / 2.0f},
         .y{GetScreenHeight() / 2.0f},
@@ -53,41 +59,6 @@ int main()
     while (!WindowShouldClose())
     {
 
-        // Ball and Paddle Movement Logic
-        ball.x += ball.speedX * GetFrameTime();
-        ball.y += ball.speedY * GetFrameTime();
-
-        if (IsKeyDown(KEY_D) && paddle1.y > 0)
-        {
-            paddle1.y -= paddle1.speed * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_F) && (paddle1.y + paddle1.height) < GetScreenHeight())
-        {
-            paddle1.y += paddle1.speed * GetFrameTime();
-        }
-
-        if (IsKeyDown(KEY_J) && paddle2.y > 0)
-        {
-            paddle2.y -= paddle2.speed * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_K) && (paddle2.y + paddle2.height) < GetScreenHeight())
-        {
-            paddle2.y += paddle2.speed * GetFrameTime();
-        }
-
-        // Ball and Paddle Collision Logic
-
-        if (ball.y < 0 || ball.y >= GetScreenHeight())
-        {
-            ball.speedY *= -1;
-        }
-
-        if (ball.x < 0 || ball.x > GetScreenWidth())
-        {
-
-            ball.speedX *= -1;
-        }
-
         // Game Over Logic
 
         if (ball.x < paddle1.x)
@@ -100,8 +71,54 @@ int main()
             std::cout << "Game Over For Player 2";
         }
 
+        // Ball Movement Logic
+        ball.x += ball.speedX * GetFrameTime();
+        ball.y += ball.speedY * GetFrameTime();
+
+        // Paddle Movement Logic
+        if (IsKeyDown(KEY_D) && paddle1.y - paddle1.height / 2 > 0)
+        {
+            paddle1.y -= paddle1.speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_F) && (paddle1.y + paddle1.height / 2) < GetScreenHeight())
+        {
+            paddle1.y += paddle1.speed * GetFrameTime();
+        }
+
+        if (IsKeyDown(KEY_J) && paddle2.y - paddle2.height / 2 > 0)
+        {
+            paddle2.y -= paddle2.speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_K) && (paddle2.y + paddle2.height / 2) < GetScreenHeight())
+        {
+            paddle2.y += paddle2.speed * GetFrameTime();
+        }
+
+        // Paddle and Ball Collision Logic
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, paddle2.GetRect()))
+        {
+            ball.speedX *= -1;
+        }
+
+         if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, paddle1.GetRect()))
+        {
+            ball.speedX *= -1;
+        }
+
+        // Boundary Collisions
+
+        if (ball.y < 0 || ball.y >= GetScreenHeight())
+        {
+            ball.speedY *= -1;
+        }
+
+        if (ball.x < 0 || ball.x > GetScreenWidth())
+        {
+
+            ball.speedX *= -1;
+        }
+
         // Rendering
-        // Game updates should be made before the BeginDrawing() call
         BeginDrawing();
         ClearBackground(BLACK);
         DrawText("Welcome to Pong!", 50, 50, 30, RED);
